8e2c2d1489d7188cd2c4119d7a314e610be7aa1f
noya <noyam1003@gmail.com>
2026-03-28 18:14:38 +0300

    added type

--- services/userManagement/src/utils/password.ts
+++ services/userManagement/src/utils/password.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 
-export const hashPassword = async (password: string) => {
+export const hashPassword = async (password: string): Promise<string> => {
   const salt: string = await bcrypt.genSalt(10);
   const encryptedPassword: string = await bcrypt.hash(password, salt);
   return encryptedPassword;
