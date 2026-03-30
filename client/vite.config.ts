db156347fb5bbb87a64f29ac51afa37f947d1eb4
annaeden1 <annaeden1@gmail.com>
2026-03-30 13:38:56 +0300

    add route of mealPlanner service to client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/mealPlanner': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
     },
   },
 })
