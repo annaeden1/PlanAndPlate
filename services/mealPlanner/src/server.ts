da5a7f9edb9995a8f7f5bcfd42a99a6179ee5339
noya <noyam1003@gmail.com>
2026-08-09 22:39:45 +0300

    fix certs

--- services/mealPlanner/src/server.ts
+++ services/mealPlanner/src/server.ts
@@ -17,8 +17,8 @@ initApp()
         );
     } else if (process.env.NODE_ENV === 'production') {
       const options = {
-        key: fs.readFileSync('../../../cs/key.pem'),
-        cert: fs.readFileSync('../../../cs/cert.pem'),
+        key: fs.readFileSync('../../../cs/myserver.key'),
+        cert: fs.readFileSync('../../../cs/CSB.crt'),
       };
       https
         .createServer(options, app)
